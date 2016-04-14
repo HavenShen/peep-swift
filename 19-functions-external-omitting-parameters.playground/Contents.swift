4e2d82066ec2606f483dde85d8113349980acddb
HavenShen <havenshen@gmail.com>
2016-04-14 16:55:20 +0800

    add functions

---
+++ 19-functions-external-omitting-parameters.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func myDevice(device: String,_ brand: String) {
+    print("Meter \(device) is \(brand)")
+}
+
+
+myDevice("Celular", "Motorola")
---
+++ 20-functions-default-parameter-values.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func sayName(personName: String = "Unknown") -> String {
+    return personName
+}
+
+print("His name is \(sayName("Haven"))")
